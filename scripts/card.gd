b784d937a943674552ebade3adb70ecbbad8b185
T-Cog <tcoghlan3@live.ca>
2024-09-25 17:31:33 -0400

    card spawn button
    wip new cards on button press

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name Card extends Node2D
 
 @export var cardName: String = "Card Name"
 @export var cardDescription: String = "Card Description"
@@ -16,19 +16,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_values()
+	set_card_values(cost_money, cost_energy, cost_time, cardName, cardDescription)
 	
 	
-func set_values(_costMoney: int, _costEnergy: int, _costTime: float, _name: String, _description: String):
-	costLabel_money.set_text("$" + str(cost_money))
-	costLabel_energy.set_text(str(cost_energy))
-	costLabel_time.set_text(str(cost_time) + "h")
+
+func set_card_values(_costM: int, _costE: int, _costT: float, _name: String, _description: String):
+	costLabel_money.set_text("$" + str(_costM))
+	costLabel_energy.set_text(str(_costE))
+	costLabel_time.set_text(str(_costT) + "h")
 	
-	nameLabel.set_text(cardName)
-	descriptionLabel.set_text((cardDescription))
+	nameLabel.set_text(_name)
+	descriptionLabel.set_text((_description))
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-
--- scripts/card_hand.gd
+++ scripts/card_hand.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var card_scene: PackedScene = preload("res://scenes/card_template.tscn")
 
+@onready var spawn_point = $CanvasLayer/Spawn
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,4 +15,12 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	card_scene.instantiate()
+	var card: Card = card_scene.instantiate()
+	spawn_point.add_child(card)
+	card.set_card_values(1, 2, 3, "spawn name", "spawn description")
+
+
+func _on_button_2_pressed():
+	var card: Card = card_scene.instantiate()
+	spawn_point.add_child(card)
+	card.set_card_values(4, 5, 6, "spawn 2", "spawn 2")
