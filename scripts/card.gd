d4f671328f2f03ebdc475315bb955ee32a8655d9
T-Cog <tcoghlan3@live.ca>
2024-09-25 19:25:52 -0400

    wip NOT STABLE
    card.gd script -
    Attempting to reassign sprite at runtime. Not currently running

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,32 +1,37 @@
 class_name Card extends Node2D
 
-@export var cardName: String = "Card Name"
-@export var cardDescription: String = "Card Description"
-@export var cost_money: int = 100
+@export var cardName: String = "Card Name" #Stores name of card
+@export var cardDescription: String = "Card Description" #Stores pop-up description
+@export var cardImage: Texture2D #Stores card art sprite 
+
+#Resource cost variables
+@export var cost_money: int = 100 
 @export var cost_energy: int = 10
 @export var cost_time: float = 1
-@export var cardImage: Node2D
 
+#Sets visual labels as variables for costs, card name, description
 @onready var costLabel_money: Label = $CostDisplay/MoneyCost/MoneyCostLabel
 @onready var costLabel_energy: Label = $CostDisplay/EnergyCost/EnergyCostLabel
 @onready var costLabel_time: Label = $CostDisplay/TimeCost/TimeCostLabel
-
 @onready var nameLabel: Label = $CardName/NameLabel
 @onready var descriptionLabel: Label = $CardDescription/DescriptionLabel
+@onready var cardImageCanvas: Sprite2D = $CardImage/CardImageSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_card_values(cost_money, cost_energy, cost_time, cardName, cardDescription)
+	set_card_values(cost_money, cost_energy, cost_time, cardName, cardDescription, cardImage)
 	
 	
 
-func set_card_values(_costM: int, _costE: int, _costT: float, _name: String, _description: String):
+#Adjusts labels and image to show costs and sprite set in-editor
+func set_card_values(_costM: int, _costE: int, _costT: float, _name: String, _description: String, _image: Texture2D):
 	costLabel_money.set_text("$" + str(_costM))
 	costLabel_energy.set_text(str(_costE))
 	costLabel_time.set_text(str(_costT) + "h")
 	
 	nameLabel.set_text(_name)
 	descriptionLabel.set_text((_description))
+	cardImageCanvas.Texture2D = _image
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/card_hand.gd
+++ scripts/card_hand.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var card_scene: PackedScene = preload("res://scenes/card_template.tscn")
+@onready var card_meds: PackedScene = preload("res://scenes/cards/Meds.tscn")
+@onready var card_work: PackedScene = preload("res://scenes/cards/Work.tscn")
 
 @onready var spawn_point = $CanvasLayer/Spawn
 
@@ -17,10 +19,10 @@ func _process(delta):
 func _on_button_pressed():
 	var card: Card = card_scene.instantiate()
 	spawn_point.add_child(card)
-	card.set_card_values(1, 2, 3, "spawn name", "spawn description")
+	#card.set_card_values(1, 2, 3, "spawn name", "spawn description", null)
 
 
 func _on_button_2_pressed():
-	var card: Card = card_scene.instantiate()
-	spawn_point.add_child(card)
-	card.set_card_values(4, 5, 6, "spawn 2", "spawn 2")
+	var workCard = card_work.instantiate()
+	spawn_point.add_child(workCard)
+	#workCard.set_card_values(4, 5, 6, "spawn 2", "spawn something new")
