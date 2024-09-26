d016aff16320de88decfea22807bbbd94b2b1c3f
T-Cog <tcoghlan3@live.ca>
2024-09-26 17:07:39 -0400

    Updated card.gd sprite swap
    card script now updates card texture as set in-editor

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,4 +1,5 @@
-class_name Card extends Node2D
+class_name Card extends Node2D 
+
 
 @export var cardName: String = "Card Name" #Stores name of card
 @export var cardDescription: String = "Card Description" #Stores pop-up description
@@ -15,23 +16,25 @@ class_name Card extends Node2D
 @onready var costLabel_time: Label = $CostDisplay/TimeCost/TimeCostLabel
 @onready var nameLabel: Label = $CardName/NameLabel
 @onready var descriptionLabel: Label = $CardDescription/DescriptionLabel
-@onready var cardImageCanvas: Sprite2D = $CardImage/CardImageSprite
+@onready var cardImageCanvas: Sprite2D = $CardImage/CardImageCanvas
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_card_values(cost_money, cost_energy, cost_time, cardName, cardDescription, cardImage)
+	set_card_values(cost_money, cost_energy, cost_time, 
+	cardName, cardDescription, cardImage)
 	
 	
 
 #Adjusts labels and image to show costs and sprite set in-editor
-func set_card_values(_costM: int, _costE: int, _costT: float, _name: String, _description: String, _image: Texture2D):
+func set_card_values(_costM: int, _costE: int, _costT: float, 
+_name: String, _description: String, _image: Texture2D):
 	costLabel_money.set_text("$" + str(_costM))
 	costLabel_energy.set_text(str(_costE))
 	costLabel_time.set_text(str(_costT) + "h")
 	
 	nameLabel.set_text(_name)
 	descriptionLabel.set_text((_description))
-	cardImageCanvas.Texture2D = _image
+	cardImageCanvas.texture = _image
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
